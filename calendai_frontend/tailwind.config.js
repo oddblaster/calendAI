130ee201b41f61202cf78776eb0941bf6e64fbc6
eren <shaoyuan00145@gmail.com>
2024-10-05 19:29:48 -0400

    working on making things pretty

--- calendai_frontend/tailwind.config.js
+++ calendai_frontend/tailwind.config.js
@@ -9,7 +9,21 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'gradient-animated': 'linear-gradient(270deg, #ff7e5f, #feb47b, #6a82fb, #fc5c7d)',
+      },
+      animation: {
+        'gradient': 'gradient-animation 5s ease infinite',
+      },
+      keyframes: {
+        'gradient-animation': {
+          '0%': { 'background-position': '0% 50%' },
+          '50%': { 'background-position': '100% 50%' },
+          '100%': { 'background-position': '0% 50%' },
+        },
+      },
+    },
   },
   plugins: [],
 }
