9fe454e3cafa461aa1e52f946856beeadbc2b4b4
eren <shaoyuan00145@gmail.com>
2024-10-05 19:25:36 -0400

    made a main page

--- calendai_frontend/app/layout.tsx
+++ calendai_frontend/app/layout.tsx
@@ -5,15 +5,17 @@
 import './globals.css';
 import { ReactNode } from 'react';
 import { SidebarProvider } from '@/contexts/SideBarContext';
+import { usePathname } from 'next/navigation';
 import Sidebar from '@/components/sidebar'; // Adjust the path as necessary
 
 export default function RootLayout({ children }: { children: ReactNode }) {
+   const pathname = usePathname();
   return (
     <html lang="en">
       <body>
         <SidebarProvider>
           <div className="flex h-screen">
-            <Sidebar />
+            {pathname != "/main" && <Sidebar />}
             <main className="flex-1 bg-gray-100 overflow-auto">
               {children}
             </main>
