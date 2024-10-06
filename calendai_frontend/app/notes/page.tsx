059c7146ce53368cb2063b6925fd0ee6d375eadb
warphi <jackangusg@gmail.com>
2024-10-06 02:42:54 -0400

    changes tasks to notes

---
+++ calendai_frontend/app/notes/page.tsx
@@ -0,0 +1,12 @@
+import Sidebar from "@/components/sidebar";
+
+const NotesPage = () => {
+    return (
+        <>
+        <div className="flex h-screen bg-gray-900 text-white">
+        </div>
+        </>
+    )
+}
+
+export default NotesPage;
